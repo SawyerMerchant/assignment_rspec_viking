90c9af3ec4d3105ac9ed362466a903530f22b5e8
James Harris <computersherpa@gmail.com>
2016-11-04 11:53:36 -0500

    11:53 AM

--- spec/warmup_spec.rb
+++ spec/warmup_spec.rb
@@ -2,12 +2,24 @@ require 'warmup'
 
 describe Warmup do
 
+let(:warm){ Warmup.new }
+
   describe '#gets_shout' do
 
     it 'Returns a string' do
-      warm = Warmup.new
-      allow(warm).to receive(:gets).and_return(str)
-      expect().to eq(0)
+      allow(warm).to receive(:gets).and_return("bark")
+      expect(warm).to receive(:gets_shout).and_return("BARK")
+      warm.gets_shout
     end
+
   end
+
+  describe '#triple_size' do
+
+    it "returns an array's length times 3" do
+      warm_double = double
+    end
+
+  end
+
 end
